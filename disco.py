eb1c057284315e8990fd238568db40ed30837a11
Lasse <36763183+L4SS3h@users.noreply.github.com>
2019-05-03 10:49:12 +0200

    Create disco.py
    Disco python

---
+++ disco.py
@@ -0,0 +1,23 @@
+from sense_hat import SenseHat
+from time import sleep
+from random import randint
+s = SenseHat()
+r = (255, 0, 0)
+g = (0, 255, 0)
+b = (0, 0, 255)
+
+def rndColour():
+    rndRed = randint(0,255)
+    rndGreen = randint(0,255)
+    rndBlue = randint(0,255)
+    return(rndRed, rndGreen, rndBlue)
+# solid color for all pixels
+# s.clear(r)
+sleeptime = 1 
+
+while True:
+  s.clear(rndColour())
+  sleep(sleeptime)
+  
+  if sleeptime > 0:
+    sleeptime -= 0.2
